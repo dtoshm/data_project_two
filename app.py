9fbe2d7658704facbfc64f12c4e6dd4efe9b0bbb
Dan McCahon <dtmccahon@outlook.com>
2023-12-28 23:11:43 +0800

    0.2 - Format
    Setup the functions and overall format of the app.

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ Project 2 - A Basketball Stats Tool
 import constants
 
 
+def clean_data():
+    print("clean_data")
+
+
+def balance_teams():
+    print("balance_teams")
+
+
+def app_start():
+    print("app_Start")
+
+
+if __name__ == "__main__":
+    clean_data()
+    balance_teams()
+    app_start()
