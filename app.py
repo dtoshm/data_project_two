b1b7950c71d5726ebf17846706336a631f89571f
Dan McCahon <dtmccahon@outlook.com>
2023-12-29 16:00:57 +0800

    0.9 - Code Refinement
    Fixed weird spaces and line breaks.

--- app.py
+++ app.py
@@ -9,30 +9,29 @@ import copy
 
 def clean_data():
     """
-        Make a deep copy, clean the data and return
+    Make a deep copy, clean the data and return
     """
     players_copy = copy.deepcopy(PLAYERS)
     players_cleaned = [{'name': player['name'], 
-                            'height': int(player['height'].split()[0]), 
-                            'guardians': player['guardians'].split(" and "), 
-                            'experience': True if player['experience'].upper() == 'YES' else False} 
-                            for player in players_copy]
+                        'height': int(player['height'].split()[0]), 
+                        'guardians': player['guardians'].split(" and "), 
+                        'experience': True if player['experience'].upper() == 'YES' else False} 
+                        for player in players_copy]
     return players_cleaned
 
 
 def balance_teams(players_copy):
     """
-        Determine the number of players for each team,
-        deep copy the teams constant, sort players by experience, 
-        assign players to teams, return the sorted teams
+    Determine the number of players for each team,
+    deep copy the teams constant, sort players by experience, 
+    assign players to teams, return the sorted teams
     """
     teams_copy = {team: [] for team in copy.deepcopy(TEAMS)}
-    
     experienced_players = [player for player in players_copy if player['experience']]
     inexperienced_players = [player for player in players_copy if not player['experience']]
-
     teams_assigned = {team: experienced_players[i::len(teams_copy)] + 
-                      inexperienced_players[i::len(teams_copy)] for i, team in enumerate(teams_copy)}
+                      inexperienced_players[i::len(teams_copy)] for i, 
+                      team in enumerate(teams_copy)}
     return teams_assigned
 
 
@@ -42,8 +41,8 @@ def display_teams(teams, user_selected_team):
     """
     players = []
     for player in teams[user_selected_team]:
-            first_name = player['name'].split()[0]
-            players.append(first_name)
+        first_name = player['name'].split()[0]
+        players.append(first_name)
     print("\nTeam: {} Stats".format(user_selected_team))
     print("-" * 20)
     print("Total Players: {}".format(len(teams[user_selected_team])))
@@ -65,25 +64,25 @@ def app_start():
     print("-" * 4 + "MENU" + "-" * 4)
     user_input = ""
     while user_input.upper() != "B":
-            print("\nHere are your choices:\n A) Display Team Stats\n B) Quit\n")
-            user_input = input("Enter an option: ")
-            if user_input.upper() == "A":
-                    user_selected_team = ""
-                    print("\nA) Panthers\nB) Bandits\nC) Warriors\n")
-                    while True:
-                            user_selected_team = input("Enter an option: ")
-                            if user_selected_team.upper() == "A":
-                                    display_teams(team_list, "Panthers")
-                            elif user_selected_team.upper() == "B":
-                                    display_teams(team_list, "Bandits")
-                            elif user_selected_team.upper() == "C":
-                                    display_teams(team_list, "Warriors")
-                            elif user_selected_team == "":
-                                    break
-                            else:
-                                    print("\nPlease Enter a Menu Option\n")
-            else:
-                    print("\nPlease Enter a Menu Option")
+        print("\nHere are your choices:\n A) Display Team Stats\n B) Quit\n")
+        user_input = input("Enter an option: ")
+        if user_input.upper() == "A":
+            user_selected_team = ""
+            print("\nA) Panthers\nB) Bandits\nC) Warriors\n")
+            while True:
+                user_selected_team = input("Enter an option: ")
+                if user_selected_team.upper() == "A":
+                    display_teams(team_list, "Panthers")
+                elif user_selected_team.upper() == "B":
+                    display_teams(team_list, "Bandits")
+                elif user_selected_team.upper() == "C":
+                    display_teams(team_list, "Warriors")
+                elif user_selected_team == "":
+                    break
+                else:
+                    print("\nPlease Enter a Menu Option\n")
+        else:
+            print("\nPlease Enter a Menu Option")
 
 
 if __name__ == "__main__":
