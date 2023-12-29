ac94f371ca9de3b5860a0e1d9aeaabd67237e008
Dan McCahon <dtmccahon@outlook.com>
2023-12-29 15:43:54 +0800

    0.7 - Display_Teams and App_start
    Added functions from previous previous python project 2.

--- app.py
+++ app.py
@@ -28,8 +28,54 @@ def balance_teams(players_copy):
     return teams_assigned
 
 
+def display_teams(teams, user_selected_team):
+        """
+        Display the players on each teams first names.
+        """
+        players = []
+        for player in teams[user_selected_team]:
+                first_name = player['name'].split()[0]
+                players.append(first_name)
+        print("\nTeam: {} Stats".format(user_selected_team))
+        print("-" * 20)
+        print("Total Players: {}".format(len(teams[user_selected_team])))
+        print("Experienced Players: {}".format(len([player for player in teams[user_selected_team] if player['experience']])))
+        print("Inexperienced Players: {}".format(len([player for player in teams[user_selected_team] if not player['experience']])))
+        height_list = [player['height'] for player in teams[user_selected_team]]
+        print("Average Height: {} {}".format(round(sum(height_list) / len(height_list), 2), "inches"))
+        print("\nPlayers on Team:")
+        print(", ".join(players))
+        print("\nPress ENTER to continue...\n")
+
+
 def app_start():
-    print("app_Start")
+        """
+        Mainmenu runs until user selects B.
+        Submenu for displaying user selected team assignments until user selects enter.
+        """
+        print("BASKETBALL TEAM STATS TOOL\n")
+        print("-" * 4 + "MENU" + "-" * 4)
+        user_input = ""
+        while user_input.upper() != "B":
+                print("\nHere are your choices:\n A) Display Team Stats\n B) Quit\n")
+                user_input = input("Enter an option: ")
+                if user_input.upper() == "A":
+                        user_selected_team = ""
+                        print("\nA) Panthers\nB) Bandits\nC) Warriors\n")
+                        while True:
+                                user_selected_team = input("Enter an option: ")
+                                if user_selected_team.upper() == "A":
+                                        display_teams(team_list, "Panthers")
+                                elif user_selected_team.upper() == "B":
+                                        display_teams(team_list, "Bandits")
+                                elif user_selected_team.upper() == "C":
+                                        display_teams(team_list, "Warriors")
+                                elif user_selected_team == "":
+                                        break
+                                else:
+                                        print("\nPlease Enter a Menu Option\n")
+                else:
+                        print("\nPlease Enter a Menu Option")
 
 
 if __name__ == "__main__":
